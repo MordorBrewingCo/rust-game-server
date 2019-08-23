2d53ff1d98e1e4cdda04ca1b46b59cd7f3c501e1
bbulla <brent.bulla@nike.com>
2019-08-23 13:47:56 -0700

    Adding variables

--- backend.tf
+++ backend.tf
@@ -2,5 +2,9 @@
 
 terraform {
   backend "s3" {
+    bucket         = "${var.backend_bucket_name}"
+    key            = "rust-server.tfstate"
+    region         = "${var.availability_zone}"
+    dynamodb_table = "${var.backend_table_name}"
   }
 }
