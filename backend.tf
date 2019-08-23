0c6dd6b286375dec7bd86fb084fd22d079e171ad
bbulla <brent.bulla@nike.com>
2019-08-23 14:01:44 -0700

    Backend config and supporting variables

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket         = "${var.backend_bucket_name}"
-    key            = "rust-server.tfstate"
+    key            = "${var.tf_state_key}"
     region         = "${var.availability_zone}"
     dynamodb_table = "${var.backend_table_name}"
   }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "availability_zone" {
     type = "string"
     default = "us-west-2a"
 }
+
+variable "tf_state_key" {
+    type = "string"
+    default = "rust-server.tfstate"
+}
+
+variable "backend_bucket_name" {
+    type = "string"
+    default = "rust-fragtopia-us-west-2-389684724582-terraform"
+}
+
+variable "backend_table_name" {
+    type = "string"
+    default = "rust-fragtopia-locktable"
+}
