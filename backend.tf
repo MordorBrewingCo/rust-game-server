8260b700e2feab41443cff1143297c6c96ec5321
bbulla <brent.bulla@nike.com>
2019-08-23 15:47:52 -0700

    Removing variables

--- backend.tf
+++ backend.tf
@@ -1,20 +1,11 @@
 ### BACKEND ###
 
-/*terraform {
+terraform {
   backend "s3" {
     bucket         = "rust-fragtopia-us-west-2-389684724582-terraform"
     encrypt        = true
     key            = "rust-server.tfstate"
     region         = "us-west-2"
     dynamodb_table = "rust-fragtopia-locktable"
-  }
-}*/
-
-terraform {
-  backend "s3" {
-    bucket         = "${var.backend_bucket_name}"
-    key            = "${var.tf_state_key}"
-    region         = "${var.availability_zone}"
-    dynamodb_table = "${var.backend_table_name}"
   }
 }
