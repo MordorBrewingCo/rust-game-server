3253fdd91a64281540c63678fe7024e1db6c17e2
bbulla <brent.bulla@nike.com>
2019-08-23 14:33:29 -0700

    Testing syntax change

--- backend.tf
+++ backend.tf
@@ -2,10 +2,9 @@
 
 terraform {
   backend "s3" {
-    bucket         = "$${var.backend_bucket_name}"
-    key            = "$${var.tf_state_key}"
-    region         = "$${var.region}"
-    dynamodb_table = "$${var.backend_table_name}"
+    bucket         = "${var.backend_bucket_name}"
+    key            = "${var.tf_state_key}"
+    region         = "${var.region}"
+    dynamodb_table = "${var.backend_table_name}"
   }
 }
-
