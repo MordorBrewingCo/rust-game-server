af9b649b44e91815e5c03517edf8d3f263f85ddc
bbulla <brent.bulla@nike.com>
2019-08-24 15:41:34 -0700

    Adding IAM roles, polices, and profiles for EC2 instance to describe volumes as part of the user-data EBS attachment

---
+++ iam.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "ec2_describe_volumes_role" {
+  name = "describe_volumes_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+resource "aws_iam_instance_profile" "ec2_describe_volumes_profile" {
+  name = "ec2_describe_volumes_profile"
+  role = "${aws_iam_role.ec2_describe_volumes_role.name}"
+}
+
+resource "aws_iam_role_policy" "ec2_describe_volumes_policy" {
+  name = "ec2_describe_volumes_policy"
+  role = "${aws_iam_role.describe_volumes_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeVolumes"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
