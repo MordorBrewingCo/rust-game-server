db7a2a2a931f285da40cd73bcca99bc3fa80420e
bbulla <brent.bulla@nike.com>
2019-08-24 17:29:33 -0700

    adding var and updating cfn template

--- ebs.tf
+++ ebs.tf
@@ -1,5 +1,5 @@
 resource "aws_ebs_volume" "rust_ebs" {
-  availability_zone = var.availability_zone
+  availability_zone = "${var.aws_region}"
   size              = 10
 }
 
@@ -8,4 +8,3 @@ resource "aws_volume_attachment" "rust_ebs_att" {
   volume_id   = aws_ebs_volume.rust_ebs.id
   instance_id = aws_instance.rust.id
 }
-
