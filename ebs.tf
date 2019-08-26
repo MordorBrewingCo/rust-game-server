caf87264a6d68982fdc88a9f60e4bf7c4213d89f
bbulla <brent.bulla@nike.com>
2019-08-26 16:20:29 -0700

    Fixing syntax

--- ebs.tf
+++ ebs.tf
@@ -6,5 +6,5 @@ resource "aws_ebs_volume" "rust_ebs" {
 resource "aws_volume_attachment" "rust_ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = "${aws_ebs_volume.rust_ebs.id}"
-  instance_id = "${$aws_instance.rust.id}"
+  instance_id = "${aws_instance.rust.id}"
 }
